1af9dbd16579e234081610aeb885f2f4fcedb09d
baoyigang <252402407@qq.com>
2017-02-24 13:51:31 +0800

    Signed-off-by: baoyigang <252402407@qq.com>

--- WCS/WindowsFormsApplication1/Form1.cs
+++ WCS/WindowsFormsApplication1/Form1.cs
@@ -21,10 +21,16 @@ namespace WindowsFormsApplication1
             SRMDataService.SRMDataService srm = new SRMDataService.SRMDataService();
             string Json = "[{\"id\":\"xcafd3ssaf\",\"taskNo\":\"1702160001\",\"taskType\":\"12\",\"taskLevel\":1,\"taskFlag\":\"2\",\"palletBarcode\":\"B001\",\"areaCode\":\"B01\",\"fromAddress\":\"B005001001\",\"toAddress\":\"B005006003\",\"status\":\"0\",\"sendDate\":\""+DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")+"\",\"sender\":\"admin\"" + "}]";
             string json = "";
-            string Ajson = "[{\"id\":\"xcafd3ssaf\",\"taskNo\":\"1702160001\",\"areaCode\":\"A01\",\"sendDate\":\"" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "\",\"sender\":\"admin\"" + "}]";
-            string a = srm.transSRMTaskAisle(Ajson);
-            string c = srm.transSRMTask(json);
-            MessageBox.Show(a);
+            //string Ajson = "[{\"id\":\"xcafd3ssaf\",\"taskNo\":\"1702160001\",\"areaCode\":\"A01\",\"sendDate\":\"" + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + "\",\"sender\":\"admin\"" + "}]";
+            //string a = srm.transSRMTaskAisle(Ajson);
+            string c = srm.transSRMTask(Json);
+            MessageBox.Show(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WCSDataService.WCSDataService wcs = new WCSDataService.WCSDataService();
+            wcs.transWCSExecuteTask("170216");
         }
     }
 }
